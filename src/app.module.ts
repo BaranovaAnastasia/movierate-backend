93e4b2902760b0365bf83bb942584c9317859e78
BaranovaAnastasia <aabaranova_3@edu.hse.ru>
2022-05-09 00:42:07 +0300

    fixes for heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { AtGuard } from './common/guards';
-import { MoviesListsModule } from './list/movies-lists.module';
 import { MovieModule } from './movie/movie.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ReviewModule } from './review/review.module';
@@ -16,8 +15,7 @@ import { UserModule } from './user/user.module';
     UserModule,
     PrismaModule,
     MovieModule,
-    ReviewModule,
-    MoviesListsModule
+    ReviewModule
   ],
   providers: [
     { provide: APP_GUARD, useClass: AtGuard }
