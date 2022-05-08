ec326e1e4e8d581b3d44ce6210792f609a9b247f
BaranovaAnastasia <aabaranova_3@edu.hse.ru>
2022-05-08 16:14:28 +0300

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true
   });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 8080);
 }
